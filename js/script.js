77bbce13349af488d33ef274bb8d2fe12d54bc4f
Jacek Knapczyk <jacekknapczyk@gmail.com>
2021-08-15 16:41:05 +0200

    done 6.3

--- js/script.js
+++ js/script.js
@@ -6,9 +6,15 @@
   });*/
 
   const titleClickHandler = function(event){
+    
+    event.preventDefault();
+    
+    const  clickedElement = this;
     console.log('Link was clicked!', event);
+
+    
   
-    /* remove class 'active' from all article links  */
+    /* [DONE] remove class 'active' from all article links  */
     
     const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -16,19 +22,33 @@
       activeLink.classList.remove('active');
     }
 
-    /* add class 'active' to the clicked link */
+    /* [IN PROGRESS] add class 'active' to the clicked link */
+    
+    clickedElement.classList.add('active');
+        console.log('clickedElement', clickedElement);
   
-    /* remove class 'active' from all articles */
+    /* [DONE] remove class 'active' from all articles */
+    
     const activeArticles = document.querySelectorAll('article.active');
 
     for(let activeArticle of activeArticles){
         activeArticle.classList.remove('active');
     }
     /* get 'href' attribute from the clicked link */
-  
+    
+    const articleSelector = clickedElement.getAttribute('href')
+        console.log('articleSelector: ' , articleSelector);
+    
     /* find the correct article using the selector (value of 'href' attribute) */
-  
+    
+    const targetArticle = document.querySelector(articleSelector);
+        console.log('targetArticle: ', targetArticle);
+    
     /* add class 'active' to the correct article */
+    
+    targetArticle.classList.add('active');
+        console.log(' add class active to the correct article', targetArticle);
+
   }
   
   const links = document.querySelectorAll('.titles a');
